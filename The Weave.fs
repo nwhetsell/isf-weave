e136bb9b19266a8c9e67bcb8f09d40488bdfd059
Nate Whetsell <nathan.whetsell@gmail.com>
2025-08-03 13:00:41 -0400

    Eliminate macros

--- The Weave.fs
+++ The Weave.fs
@@ -42,31 +42,26 @@
 }
 */
 
-#define fragColor gl_FragColor
-#define fragCoord gl_FragCoord.xy
-#define iResolution RENDERSIZE
-#define iTime TIME
-
 #define PI 3.1415926535897932384626433832795
 vec3 cmap(float x) { return pow(.5+.5*cos(PI * x + vec3(1,2,3)), vec3(2.5)); }
 
 void main()
 {
-    vec2 uv = (2. * fragCoord-iResolution.xy)/iResolution.y;
-    vec3 ro = vec3(0,0,iTime);
+    vec2 uv = (2. * gl_FragCoord.xy-RENDERSIZE)/RENDERSIZE.y;
+    vec3 ro = vec3(0,0,TIME);
     vec3 rd = normalize(vec3(uv, -focal));
     vec3 color = vec3(0);
     float t = 0.;
     for (int i = 0; i < 99; i++) {
         vec3 p = t * rd + ro;
 
-        float T = (t+iTime)/twist;
+        float T = (t+TIME)/twist;
         float c = cos(T), s = sin(T);
         p.xy = mat2(c,-s,s,c) * p.xy;
 
         for (float f = 0.; f < 9.; f++)  {
             float a = exp(f)/exp2(f);
-            p += cos(p.yzx * a + iTime)/a;
+            p += cos(p.yzx * a + TIME)/a;
         }
         float d = 1./saturation + abs((ro -p-vec3(0,1,0)).y-1.)/resolution;
         color += cmap(t) * 2e-3 / d;
@@ -76,5 +71,5 @@ void main()
     color *= color*color;
     color = 1.-exp(-color);
     color = pow(color, vec3(1./2.2));
-    fragColor = vec4(color, 1);
+    gl_FragColor = vec4(color, 1);
 }
